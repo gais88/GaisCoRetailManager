10a568cd3c51f97a82f54a0c16648b13d7a062de
Saab Saab <saab_88@outlook.com>
2022-10-20 23:52:08 +0300

    Add WPF Project config the DI Add Calliburn micro framework

---
+++ GRMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,51 @@
+﻿using Caliburn.Micro;
+using GRMDesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GRMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer _container = new SimpleContainer();
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                     .Where(x => x.IsClass)
+                     .Where(x => x.Name.EndsWith("ViewModel"))
+                     .ToList()
+                     .ForEach(modelViewType => _container.RegisterPerRequest(
+                         modelViewType, modelViewType.ToString(), modelViewType));
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
