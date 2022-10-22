85aa271687ff23bfa4f1c32fe06241ae9d20a85c
Saab Saab <saab_88@outlook.com>
2022-10-22 17:47:04 +0300

    add login form and fix passordbox issue

--- GRMDesktopUI/ViewModels/ShellViewModel.cs
+++ GRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 
 namespace GRMDesktopUI.ViewModels
 {
-    public class ShellViewModel 
+    public class ShellViewModel :Conductor<object>
     {
+        private LoginViewModel _LoginVM;
+        public ShellViewModel(LoginViewModel loginVM)
+        {
+            _LoginVM = loginVM;
+            ActivateItemAsync(_LoginVM);
+        }
+
+        
     }
 }
